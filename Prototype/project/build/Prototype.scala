773d2932a2331517a88b3d71a681137550e6aa6f
Brian Howard <bhoward@depauw.edu>
2010-06-10 11:26:45 -0400

    Added sxr to build

--- Prototype/project/build/Prototype.scala
+++ Prototype/project/build/Prototype.scala
@@ -1,5 +1,11 @@
 import sbt._
 
-class PrototypeProject(info: ProjectInfo) extends DefaultProject(info) {
+class PrototypeProject(info: ProjectInfo) extends DefaultProject(info) with AutoCompilerPlugins
+{
   val specs = "org.scala-tools.testing" % "specs" % "1.6.2.1"
+  val sxr = compilerPlugin("org.scala-tools.sxr" %% "sxr" % "0.2.4")
+  override def compileOptions =
+    CompileOption("-P:sxr:base-directory:" + mainScalaSourcePath.absolutePath) ::
+    super.compileOptions.toList
+
 }
