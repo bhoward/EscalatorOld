e92ae63746e2e59241350c267a3feb66f3031aa5
Brian Howard <bhoward@depauw.edu>
2010-06-11 13:30:12 -0400

    Fragment comments in source

--- Prototype/src/main/scala/Factorial.scala
+++ Prototype/src/main/scala/Factorial.scala
@@ -1,8 +1,10 @@
 package edu.depauw
 
+//<Factorial>
 /**
  * A non-OO object to compute factorial.  It's not good FP either...
  */
 object Factorial {
   def apply(n: Int): BigInt = if (n <= 1) 1 else Factorial(n - 1) * n
 }
+//</Factorial>
